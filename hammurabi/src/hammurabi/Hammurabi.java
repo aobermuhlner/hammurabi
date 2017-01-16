aeaf2046e247d3156fb5074910ce550162cb6a78
Adrian <Adrian@AdrianLaptop>
2017-01-16 14:12:59 +0100

    rounded most of the numbers, birthrate is way to low

--- hammurabi/src/hammurabi/Hammurabi.java
+++ hammurabi/src/hammurabi/Hammurabi.java
@@ -11,11 +11,13 @@ public class Hammurabi {
 	boolean finished = false;
 	int feed = 0;
 	int seed = 0;
+	int acresChange = 0;
 	double harvest = 0;
 	double populationDeath = 0;
 	double populationBirth = 0;
 	double plagueDeath = 0;
 	double ratStorageEaten = 0;
+	double acresPrice = 25;
 	Scanner sc = new Scanner(System.in);
 	private void runGame() {
 
@@ -31,22 +33,28 @@ public class Hammurabi {
 
 	}
 	private void printStatus() {
-		System.out.println();
 		System.out.println("Year = " + year);
 		System.out.println("Population = " + population);
 		System.out.println("Acres = " + acres);
 		System.out.println("Storage = " + storage + " bushels");
+		System.out.println("The price for one acres right now is " + acresPrice);
 	}
 	
 	private void getOrders() {
 		boolean valid = false;
 		do {
+			System.out.println("How many acres do you want to buy?");
+			acresChange = sc.nextInt();
 			System.out.println("How much do you want to feed to your population?");
 			feed = sc.nextInt();
 			System.out.println("How many aceres do you want to seed, with bushels?");
 			seed = sc.nextInt();
 
 			valid = true;
+			if(acresChange > acres){
+				System.out.println("You only have " + acres + " acres");
+				valid = false;
+			}
 			if(seed + feed > storage){
 				System.out.println("Your storage has only " + storage + " bushels.");
 				valid = false;
@@ -60,15 +68,17 @@ public class Hammurabi {
 				valid = false;
 			}
 		} while(!valid);
-		
+		System.out.println();
 	}
 	
 	private void calculateEffects() {
+		acres	+= acresChange;
+		storage += -acresChange * acresPrice;
 		storage -= feed;
 		storage -= seed;
-		harvest = seed * (Math.random() * 6 + 2);
+		harvest = seed * (Math.random() * 4 + 1);
 		storage += harvest;
-		
+		harvest = Math.round(harvest*100)/100.0; 
 		int populationDelta = feed / FEED_PER_PERSON - population;
 		if(populationDelta < 0){
 			populationDeath = -populationDelta;
@@ -76,22 +86,26 @@ public class Hammurabi {
 			populationDeath = 0;
 		}
 		
-		population -= populationDeath;
-		populationBirth = population * (Math.random() * 0.1 + 0.04);
-		population += populationBirth;
+			population -= populationDeath;
+			populationBirth = population * (Math.random() * 0.1 + 0.05);
+			population += populationBirth;
+		acresPrice = 100 * (Math.random() * 0.1 + 0.15);
+		acresPrice = Math.round(acresPrice*100)/100.0; 
 	}
-	
 	private void calculateCatastroph() {
-		if(Math.random() < 0.2){
+		if(Math.random() < 0.1){
 			plagueDeath = population * (Math.random() * 0.1 + 0.4);
+			plagueDeath = Math.round(plagueDeath*100)/100.0; 
 			population -= plagueDeath;
 		} else {
 			plagueDeath = 0;
 		}
-		if(Math.random() < 0.2){
+		if(Math.random() < 0.5){
 			ratStorageEaten = storage * (Math.random() * 0.05 + 0.15);
+			ratStorageEaten = Math.round(ratStorageEaten*100)/100.0; 
 			storage -= ratStorageEaten;
 		}
+		else ratStorageEaten = 0;
 	}
 	
 	private void printEffects() {
@@ -101,6 +115,10 @@ public class Hammurabi {
 		if(plagueDeath > 0){
 			System.out.println("The plague killed " + plagueDeath + " people");			
 		}
+		if(ratStorageEaten > 0){
+			System.out.println("Rats have eaten " + ratStorageEaten + " bushels of your storage");
+		}
+		
 	}
 
 
